bc9d1978a9f19bcca69d88ed37a2667b58bb0912
Wallaby <avocado34.131@gmail.com>
2023-02-28 23:54:24 +0900

    Initial Commit

---
+++ WantedPreOnboarding/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WantedPreOnboarding
+//
+//  Created by 이승기 on 2023/02/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
